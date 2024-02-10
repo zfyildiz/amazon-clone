8ea2dd1f427e0d1aeada78e39d0eaa66da4e875a
“zfyildiz” <feyzayildiz.dev@gmail.com>
2024-02-10 20:58:41 +0300

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,12 @@
-export const cart = [
-
-];
+export const cart = [{
+    productId: '3fdfe8d6-9a15-4979-b459-585b0d0545b9',
+    quantity: 1
+    },
+    {
+    productId: 'c2a82c5e-aff4-435f-9975-517cfaba2ece',
+    quantity: 2
+    },
+    ];
 
 export function addToCart(productId) {
     let matchingItem;
